e4b49107e56aab06774228449653939ad4fc4c59
dove <obenedicta4@gmail.com>
2024-01-15 23:27:15 +0100

    edit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    .then(([response]) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[0].lastName}`);
     })
     .catch((error) => {
       console.error('Signup system offline', error);
