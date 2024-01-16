7faaa9248ec8a5d8d8a44c504cb74fa2068817e9
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-16 21:04:45 +0100

    Create 8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](opt) {
+    if (opt === 'number') {
+      return this._size;
+    }
+    if (opt === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+} 
