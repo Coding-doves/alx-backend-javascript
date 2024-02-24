2c67694ea33f7761047b802dacf864170f3946e6
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-24 11:19:37 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
   const iterator = {
     [Symbol.iterator]: function* () {
-      for (const dept in report.allEmployees) {
+      for ( const dept in report.allEmployees ) {
         const employeesInDepartment = report.allEmployees[dept];
-        for (const employee of employeesInDepartment) {
+        for ( const employee of employeesInDepartment ) {
           yield employee;
         }
       }
