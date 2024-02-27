b8428df363c310b3e1c2398a9e0e4f86c55438c6
dove <obenedicta4@gmail.com>
2024-02-27 07:52:47 +0100

    updating file 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const countStudents = ((path) => {
+const countStudents = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
@@ -8,36 +8,34 @@ const countStudents = ((path) => {
         return;
       }
 
-      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const lines = data.trim().split('\n').slice(1);
       const fieldCnt = {};
       const fields = {};
       let totStudent = 0;
 
-      for (let i = 1; i < lines.length; i += 1) {
-        const col = lines[i].split(',');
-        const fName = col[0];
-        const field = col[3];
+      for (const line of lines) {
+        const [fName, , , field] = line.split(',').map(item => item.trim());
 
         if (field in fieldCnt) {
-          fieldCnt[field] += 1;
+          fieldCnt[field]++;
           fields[field].push(fName);
         } else {
           fieldCnt[field] = 1;
           fields[field] = [fName]
         }
-        totStudent += 1;
+        totStudent++;
       }
 
       console.log(`Number of students: ${totStudent}`);
 
       for (const field in fieldCnt) {
-        if (field) {
+        if (fieldCnt.hasOwnProperty(field)) {
           console.log(`Number of students in ${field}: ${fieldCnt[field]}. List: ${fields[field].join(',')}`);
         }
       }
       resolve();
     });
   });
-});
+};
 
 module.exports = countStudents;
