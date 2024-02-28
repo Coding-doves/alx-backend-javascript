dca72da391bbc6d14bf254ce2ba41d8100565158
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-28 08:37:20 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
-/** 
+/**
  * create an HTTP server
  */
 
 const http = require('http');
 
-const hostname  = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 1245;
 
-//creatin the server
+// creatin the server
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
-  //set header
+  // set header
   response.setHeader('Content-Type', 'text/plain');
   // Response body
   response.end('Hello Holberton School!');
