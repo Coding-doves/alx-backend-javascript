0d23b171317c74e8330e3db38dfab41bbd5ebd2a
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-29 17:47:22 +0100

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,11 +2,11 @@ function calculateNumber(type, a, b) {
   let total = 0;
   switch (type) {
     case 'SUM':
-	    total = Math.round(a) + Math.round(b);
-	    break;
+      total = Math.round(a) + Math.round(b);
+      break;
     case 'SUBTRACT':
-	    total = Math.round(a) - Math.round(b);
-	    break;
+      total = Math.round(a) - Math.round(b);
+      break;
     case 'DIVIDE':
       if (Math.round(b) === 0) {
         total = "Error";
