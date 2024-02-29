e3beb21fae618de5dce667f0d211159846557f33
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-29 18:39:58 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,22 +4,22 @@ const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function() {
 
-  it(`checking if numbers round`, function() {
+  it(`sum and round`, function() {
     assert.strictEqual(calculateNumber("SUM", 1, 2), 3);
   });
-  it(`checking if numbers round`, function() {
+  it(`sum and round numbers`, function() {
     assert.strictEqual(calculateNumber("SUM", 1.4, 2.2), -1);
   });
-  it(`checking if numbers round`, function() {
+  it(`sub and round numbers`, function() {
     assert.strictEqual(calculateNumber("SUBTRACT", 4.9, 2.7), 2);
   });
-  it(`checking if numbers round`, function() {
+  it(`sum and round numbers`, function() {
     assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
   });
-  it(`checking if numbers round`, function() {
+  it(`div and round numbers`, function() {
     assert.strictEqual(calculateNumber("DIVIDE", 1.7, 0), "Error");
   });
-  it(`checking if numbers round`, function() {
+  it(`div and round numbers`, function() {
     assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.6), 0.2);
   });
 });
