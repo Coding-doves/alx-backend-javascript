4f1635647c352b992023d15cd45feeef245f21ea
dove <obenedicta4@gmail.com>
2024-02-29 15:20:59 +0100

    update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,27 +1,27 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
 const {it, describe} = require("mocha");
 
-describe('calculateNumber', () => {
-  it('return the sum of whole numbers', ( () => {
-    assert.strictEqual(calculate(1, 2) ,3);
-  }));
-  it('return the sum of whole negative numbers', ( () => {
-    assert.strictEqual(calculate(-1, -2) ,-3);
-  }));
-  it('return the sum of rounded numbers', ( () => {
-    assert.strictEqual(calculate(1.4, 2.6) ,4);
-  }));
-  it('return the sum of negative and positive rounded numbers', ( () => {
-    assert.strictEqual(calculate(-1.4, 2.6) ,1);
-  }));
-  it('return the sum of zeros', ( () => {
-    assert.strictEqual(calculate(0, 0) ,0);
-  }));
-  it('return the sum of rounded numbers', ( () => {
-    assert.strictEqual(calculate(-1.4, -2.6) ,-4);
-  }));
-  it('return the sum of rounded numbers', ( () => {
-    assert.strictEqual(calculate(1, 3.6) ,4);
-  }));
+describe('calculateNumber()', () => {
+  it('return the sum of whole numbers', () => {
+    assert.strictEqual(calculateNumber(1, 2) ,3);
+  });
+  it('return the sum of whole negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, -2) ,-3);
+  });
+  it('return the sum of rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.6) ,4);
+  });
+  it('return the sum of negative and positive rounded numbers', () => {
+    assert.strictEqual(calculateNumber(-1.4, 2.6) ,2);
+  });
+  it('return the sum of zeros', () => {
+    assert.strictEqual(calculateNumber(0, 0) ,0);
+  });
+  it('return the sum of rounded numbers', () => {
+    assert.strictEqual(calculateNumber(-1.4, -2.6) ,-4);
+  });
+  it('return the sum of whole and rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3.6) ,5);
+  });
 });
