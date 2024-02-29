2663246ae67fee2975e5574a0c7cda44f24bc381
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-29 17:09:52 +0100

    Create 1-calcul.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+function calculateNumber(type, a, b) {
+  let total = 0;
+  switch (type) {
+    case 'SUM':
+	    total = Math.round(a) + Math.round(b);
+	    break;
+    case 'SUBTRACT':
+	    total = Math.round(a) - Math.round(b);
+	    break;
+    case 'DIVIDE':
+      if (Math.round(b) === 0) {
+        total = "Error";
+      } else {
+        total = Math.round(a) / Math.round(b);
+      }
+      break;
+  }
+  return total;
+}
+
+module.exports = calculateNumber;
